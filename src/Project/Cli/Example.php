6a5afbcd33826928cdb9182de391fb3546322759
Brooke Bryan <brooke@justdevelop.it>
2013-04-17 15:11:51 +0100

    Text Table example

--- src/Project/Cli/Example.php
+++ src/Project/Cli/Example.php
@@ -7,6 +7,8 @@ namespace Project\Cli;
 
 use Cubex\Cli\CliCommand;
 use Cubex\Cli\Shell;
+use Cubex\FileSystem\FileSystem;
+use Cubex\Text\TextTable;
 
 class Example extends CliCommand
 {
@@ -26,6 +28,23 @@ class Example extends CliCommand
       Shell::COLOUR_FOREGROUND_LIGHT_CYAN
     );
     $this->_cliOut();
+  }
+
+  public function table()
+  {
+    $table = new TextTable();
+    $table->setColumnHeaders('Date', 'Value', 'Third', 'Random');
+    for($i = 0; $i < 10; $i++)
+    {
+      $table->appendRow(
+        date("Y-m-d H:i:s", rand(100000, time())),
+        ('Random Data ' . FileSystem::readRandomCharacters(5)),
+        'Third Column',
+        FileSystem::readRandomCharacters(30)
+      );
+    }
+
+    echo $table;
   }
 
   protected function _cliOut()
